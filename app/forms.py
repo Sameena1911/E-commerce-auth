8a11c6de02c65b55ca4a675af6dd4dd8e4c356e1
Sameena1911 <sameenaferoz8@gmail.com>
2024-12-18 10:38:07 +0530

    Initial commit

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password', message='Passwords must match.')
+    ])
+    role = SelectField('Role', choices=[('customer', 'Customer'), ('delivery', 'Delivery')], validators=[DataRequired()])
+    contact = StringField('Contact', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    dob = DateField('Date of Birth', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
